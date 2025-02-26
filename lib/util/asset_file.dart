abc787f6291e4d709d1ef442353de6b23855396e
samuelkanyi <amshelkanyi@gmail.com>
2025-02-27 02:22:25 +0300

    add AssetFile class for managing guide screen asset paths

---
+++ lib/util/asset_file.dart
@@ -0,0 +1,5 @@
+class AssetFile{
+  static const String guideScreen1 = 'assets/illustrations/guide_screen_1.svg';
+  static const String guideScreen2 = 'assets/illustrations/guide_screen_2.svg';
+  static const String guideScreen3 = 'assets/illustrations/guide_screen_3.svg';
+}
