1a15b3a36a7da8c710e024c6c0aa2be5aad9e945
Samuel Kanyi <amshelhack3r@gmail.com>
2025-02-28 00:57:10 +0300

    add logging setup to main for improved debugging

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,16 @@
 import 'package:expense_tracker/app/app.dart';
 import 'package:expense_tracker/bootstrap.dart';
+import 'package:logger/logger.dart';
 
 void main() {
+  _setupLogging();
   bootstrap(() => const App());
+}
+
+void _setupLogging() {
+  Logger.level = Level.all;
+  Logger.addLogListener((logEvent){
+      print('${logEvent.level.name}: ${logEvent.time}: ${logEvent.message}');
+    
+  });
 }
