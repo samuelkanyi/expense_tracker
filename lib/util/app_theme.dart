0cdc9d4b5e58206b0a5652382300ef11a9cfe516
Samuel Kanyi <amshelhack3r@gmail.com>
2025-03-04 23:03:53 +0300

    add: enhance appTheme with Material 3 support and comprehensive text styles

--- lib/util/app_theme.dart
+++ lib/util/app_theme.dart
@@ -1,6 +1,78 @@
+import 'package:expense_tracker/util/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final ThemeData appTheme = ThemeData(
   fontFamily: GoogleFonts.inter().fontFamily,
+  useMaterial3: true,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+    ),
+    displayMedium: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+    displaySmall: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    bodyLarge: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    bodyMedium: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.normal,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+    ),
+    headlineLarge: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    headlineSmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.normal,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+    ),
+    titleSmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.normal,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+    labelSmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  buttonTheme: ButtonThemeData(
+    height: 50,
+    buttonColor: AppColors.violet100,
+    textTheme: ButtonTextTheme.primary,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+  ),
 );
