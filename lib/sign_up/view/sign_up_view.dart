8978ee3f0bafacff997fa8bee5780ac75af03af8
Samuel Kanyi <amshelhack3r@gmail.com>
2025-03-04 23:03:40 +0300

    add: enhance SignUpView with custom input fields and Google sign-up option

--- lib/sign_up/view/sign_up_view.dart
+++ lib/sign_up/view/sign_up_view.dart
@@ -1,10 +1,15 @@
 import 'package:bloc/src/cubit.dart';
 import 'package:expense_tracker/sign_up/cubit/cubit/sign_up_cubit.dart';
+import 'package:expense_tracker/util/asset_file.dart';
+import 'package:expense_tracker/util/colors.dart';
 import 'package:expense_tracker/util/common/app_header.dart';
+import 'package:expense_tracker/util/common/base_button.dart';
+import 'package:expense_tracker/util/common/custom_input.dart';
 import 'package:expense_tracker/util/dimensions.dart';
 import 'package:expense_tracker/widget/cubit_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignUpView extends CubitWidget<SignUpCubit, SignUpState> {
   @override
@@ -13,37 +18,74 @@ class SignUpView extends CubitWidget<SignUpCubit, SignUpState> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: Dimensions.paddingL),
-          child: Column(children: [
-            AppHeader(),
-            SizedBox(height: 20),
-            const Text('Sign Up', style: TextStyle(fontSize: 20)),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+          child: Column(spacing: Dimensions.paddingM, children: [
+            AppHeader(
+              label: 'Sign Up',
+            ),
+            SizedBox(height: Dimensions.custom56),
+            CustomInput(
+              controller: TextEditingController(),
+              onChanged: (String txt) {},
+              labelText: 'Name',
+            ),
+            CustomInput(
+              controller: TextEditingController(),
+              onChanged: (String txt) {},
+              hintText: 'Email',
+              labelText: 'Email',
+            ),
+            CustomInput(
+              controller: TextEditingController(),
+              onChanged: (String txt) {},
+              hintText: 'Password',
+              labelText: 'Password',
+              isPassword: true,
+              hasSuffixIcon: true,
+            ),
+            //terms and agreement
+            Row(
+              children: [
+                Checkbox(value: false, onChanged: (bool? value) {}),
+                const Text('I agree to the terms and conditions'),
+              ],
+            ),
+            PrimaryButton(
+              onPressed: () {},
+              label: 'Sign Up',
+              width: double.infinity,
+            ),
+            Text('Or with',
+                style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                      color: AppColors.baseLight20,
+                    )),
+            TextButton.icon(
+              onPressed: () {},
+              label: Text('Sign up with Google',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium
+                      ?.copyWith(fontWeight: FontWeight.w700)),
+              icon: SvgPicture.asset(
+                AssetFile.googleIcon,
+              ),
+              style: TextButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(Dimensions.custom10),
+                ),
+                side: const BorderSide(color: AppColors.baseLight20),
+                minimumSize: Size(double.infinity, 50),
               ),
             ),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // cubit.signUp();
-              },
-              child: const Text('Sign Up'),
+            //already have an account
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Already have an account?'),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Login'),
+                ),
+              ],
             ),
           ]),
         ),
