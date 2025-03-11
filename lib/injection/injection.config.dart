134e563ba8f102bc1ecd9d6d8b8b61e4a602327d
Samuel Kanyi <amshelhack3r@gmail.com>
2025-03-11 20:24:59 +0300

    add injection

--- lib/injection/injection.config.dart
+++ lib/injection/injection.config.dart
@@ -9,11 +9,15 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:expense_tracker/login/cubit/cubit/login_cubit.dart';
-import 'package:expense_tracker/sign_up/cubit/cubit/sign_up_cubit.dart'
-    as _i1016;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:supabase_flutter/supabase_flutter.dart' as _i454;
+
+import '../app/repository/sign_up_repository.dart' as _i448;
+import '../app/repository/sign_up_repository_impl.dart' as _i555;
+import '../login/cubit/cubit/login_cubit.dart' as _i780;
+import '../sign_up/cubit/cubit/sign_up_cubit.dart' as _i637;
+import 'injection.dart' as _i464;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -26,8 +30,15 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i1016.SignUpCubit>(() => _i1016.SignUpCubit());
-    gh.factory<LoginCubit>(() => LoginCubit());
+    final externalDependenciesModule = _$ExternalDependenciesModule();
+    gh.factory<_i780.LoginCubit>(() => _i780.LoginCubit());
+    gh.singleton<_i454.SupabaseClient>(
+        () => externalDependenciesModule.supabaseClient);
+    gh.factory<_i448.SignUpRepository>(() => _i555.SignUpRepositoryImpl());
+    gh.factory<_i637.SignUpCubit>(
+        () => _i637.SignUpCubit(gh<_i448.SignUpRepository>()));
     return this;
   }
 }
+
+class _$ExternalDependenciesModule extends _i464.ExternalDependenciesModule {}
