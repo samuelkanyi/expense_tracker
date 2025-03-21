153af3a6915f6f26134edadebcb9949f66cbdef0
Samuel Kanyi <amshelhack3r@gmail.com>
2025-03-21 08:43:28 +0300

    housekeeping

--- lib/l10n/arb/app_localizations_es.dart
+++ lib/l10n/arb/app_localizations_es.dart
@@ -12,17 +12,23 @@ class AppLocalizationsEs extends AppLocalizations {
   String get guide_screen_1_title => 'Gain total control of your money';
 
   @override
-  String get guide_screen_1_desc => 'Become your own money manager and make every cent count';
+  String get guide_screen_1_desc =>
+      'Become your own money manager and make every cent count';
 
   @override
   String get guide_screen_2_title => 'Know where your money goes';
 
   @override
-  String get guide_screen_2_desc => 'Track your transaction easily, with categories and financial report';
+  String get guide_screen_2_desc =>
+      'Track your transaction easily, with categories and financial report';
 
   @override
   String get guide_screen_3_title => 'Planning ahead';
 
   @override
-  String get guide_screen_3_desc => 'Setup your budget for each category so you in control';
+  String get guide_screen_3_desc =>
+      'Setup your budget for each category so you in control';
+
+  @override
+  String get login_failed => 'Login Failed';
 }
